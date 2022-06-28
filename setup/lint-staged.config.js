05c333a64b72796dc01674987622853f543938d0
enoatu <acro1452@gmail.com>
2022-06-29 01:11:05 +0900

    :wrench: lint順番変更

--- setup/lint-staged.config.js
+++ setup/lint-staged.config.js
@@ -1,9 +1,9 @@
 const exec = (dir, cmd) => `bash -c "cd ../${dir} && ${cmd}"`
 module.exports = {
   '../front/src/**/*{.js,.jsx,.ts,.tsx,.d.ts,.mts}': (filenames) => [
-    exec('front', 'yarn tsc'),
     exec('front', `yarn prettier --check --write ${filenames.join(' ')}`),
     exec('front', `yarn eslint --fix ${filenames.join(' ')}`),
+    exec('front', 'yarn tsc'),
   ],
   '../app/randomfield/**/*.go': () => exec('app', 'docker-compose exec -T app gofmt -w .'),
   '../terraform/**/*.tf': () => exec('terraform', 'terraform fmt -write=true ../terraform'),
